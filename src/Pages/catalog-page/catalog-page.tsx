ab3e09c8af758e2fb5fc35be5880fdabeba86218
Vladislav Maslakov <maslakov20@mail.ru>
2025-03-12 17:31:37 +0500

    Add catalog page

--- src/Pages/catalog-page/catalog-page.tsx
+++ src/Pages/catalog-page/catalog-page.tsx
@@ -5,13 +5,67 @@ export default function CatalogPage(): JSX.Element {
   return (
     <>
       <HeaderAuth/>
-      <main>
+      <main className='catalog'>
         <section>
           <input type="text" placeholder='Поиск'/>
           <button>Избранное</button>
         </section>
         <section className='flex'>
-          <div>СПИСОК БОКСОВ</div>
+          <div>
+            <div className='card-fitnessbox flex'>
+              <img src="../img/FitnessBox.png" alt="" className='card-fitnessbox-img'/>
+              <div className='card-fitnessbox-text flex'>
+                <h2 className='card-fitnessbox-title'>FitnessBox1</h2>
+                <p className='card-fitnessbox-p'>Менделеева 11</p>
+                <p className='card-fitnessbox-p'>Количество посещений: 568</p>
+              </div>
+              <div className='card-fitnessbox-btns flex'>
+                <button>
+                  <img src="../img/Star.svg" alt="" />
+                </button>
+                <div>
+                  <img src="" alt="" />
+                  <p className='card-fitnessbox-p'>4/5</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='card-fitnessbox flex'>
+              <img src="../img/FitnessBox.png" alt="" className='card-fitnessbox-img'/>
+              <div className='card-fitnessbox-text flex'>
+                <h2 className='card-fitnessbox-title'>FitnessBox1</h2>
+                <p className='card-fitnessbox-p'>Менделеева 11</p>
+                <p className='card-fitnessbox-p'>Количество посещений: 568</p>
+              </div>
+              <div className='card-fitnessbox-btns flex'>
+                <button>
+                  <img src="../img/Star.svg" alt="" />
+                </button>
+                <div>
+                  <img src="" alt="" />
+                  <p className='card-fitnessbox-p'>4/5</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='card-fitnessbox flex'>
+              <img src="../img/FitnessBox.png" alt="" className='card-fitnessbox-img'/>
+              <div className='card-fitnessbox-text flex'>
+                <h2 className='card-fitnessbox-title'>FitnessBox1</h2>
+                <p className='card-fitnessbox-p'>Менделеева 11</p>
+                <p className='card-fitnessbox-p'>Количество посещений: 568</p>
+              </div>
+              <div className='card-fitnessbox-btns flex'>
+                <button>
+                  <img src="../img/Star.svg" alt="" />
+                </button>
+                <div>
+                  <img src="" alt="" />
+                  <p className='card-fitnessbox-p'>4/5</p>
+                </div>
+              </div>
+            </div>
+          </div>
           <div>ЯНДЕКС КАРТА</div>
         </section>
       </main>
--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -3,6 +3,7 @@ export const fitnessBoxes = {
     name: 'FitnessBox-1',
     imagePreview: '',
     idLock: '',
+    adress: 'Менделеева 11',
     location: ['56.42342134', '42.3242134231'],
     reviews: ['Норм', 'Норм', 'Норм', 'Норм', 'Норм'],
   }
